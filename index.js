3ebe154b5ba8e868697bb7298095f499ddbc1db1
OP <omprakashr206@gmail.com>
2022-06-22 13:51:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function f1() {
+    var x = document.getElementById("pass");
+    if (x.value === "op987654321") {
+        document.write("hotspot ok");
+    }
+    else
+        document.write("no hotspot for u :(");
+}
